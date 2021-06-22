ac996d42acf2da0f00bff3c4c6c3f2e900bdaea8
RuthvikCh <chennapragadaruthvik@gmail.com>
2021-06-22 21:28:17 +0530

    Provided tv search opt and dload pages for tv series

--- files/scripttv.js
+++ files/scripttv.js
@@ -7,7 +7,7 @@ const API_KEY = 'api_key=7a31ef8faf51733740c3af33cdc42c4a';
 const BASE_URL = 'https://api.themoviedb.org/3';
 const API_URL = BASE_URL + '/discover/movie?sort_by=popularity.desc&'+API_KEY;
 const IMG_URL = 'https://image.tmdb.org/t/p/w500';
-const searchURL = BASE_URL + '/search/movie?'+API_KEY;
+const searchtvURL = BASE_URL + '/search/tv?'+API_KEY;
 const APIS_URL = BASE_URL + '/discover/tv?sort_by=popularity.desc&'+API_KEY;
 
 
@@ -85,7 +85,7 @@ formSearch.addEventListener('submit', (e) => {
     const searchTerm = search.value;
 
     if(searchTerm) { 
-        getSeries(searchURL+'&query='+searchTerm)
+        getSeries(searchtvURL+'&query='+searchTerm)
     }else{
         getSeries(APIS_URL);
     }
